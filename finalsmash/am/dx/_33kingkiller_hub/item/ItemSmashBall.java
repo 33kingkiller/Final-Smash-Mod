9bed96576c7fb459fae6dba77f9b36f8f0dfcc19
BuildTools <unconfigured@null.spigotmc.org>
2015-06-18 01:38:25 -0500

    Revert "I believe they call this Genesis?"
    This reverts commit 781388a9a4c2444cb29874e34f915173e0363dc8.

--- finalsmash/am/dx/_33kingkiller_hub/item/ItemSmashBall.java
+++
@@ -1,7 +0,0 @@
-package am.dx._33kingkiller_hub.item;
-
-import net.minecraft.item.Item;
-
-public class ItemSmashBall extends Item {
-
-}
--- finalsmash/am/dx/_33kingkiller_hub/main/ServerSidedProxy.java
+++
@@ -1,9 +0,0 @@
-package am.dx._33kingkiller_hub.main;
-
-public class ServerSidedProxy {
-	
-	//Registers proxy stuff for the server side.
-	public void RegisterRenderer() {
-		
-	}
-}
